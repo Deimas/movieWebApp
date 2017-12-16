2715b1490da2bf42c857045fc5d6be6daca7e1ed
Dmitry Bogdanov <bogdanov@phie.ru>
2017-12-16 13:48:34 +0300

    refactor

--- routes/notFound.js
+++ routes/notFound.js
@@ -2,16 +2,7 @@ const render = require('../lib/render');
 
 
 function notFound(req, res) {
-	render('error.html', { error: 'Не найдено' }, (error, html) => {
-		if (error) {
-			res.writeHead(500, { 'Content-Type': 'text/plain'});
-			return res.end(error.message);
-		}
-
-		res. statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(html);
-	});
+	render(res, 'error.html', { error: 'Не найдено' });
 }
 
 module.exports = notFound;
